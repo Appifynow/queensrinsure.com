025981353332bc258b2e3736ccf3c2e8e390e531
dcoollx <dsqjr@msn.com>
2026-05-22 17:15:38 -0400

    remove header

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Calendar, MessageSquare } from "lucide-react";
+import { MessageSquare } from "lucide-react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
